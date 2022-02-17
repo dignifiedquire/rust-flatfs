7a3a2be6e96ba239e9390a333c9575c26ade2ebd
dignifiedquire <me@dignifiedquire.com>
2022-02-17 12:17:42 +0100

    expand read example

--- examples/read.rs
+++ examples/read.rs
@@ -1,11 +1,12 @@
 use std::env;
 
-use eyre::Result;
+use eyre::{eyre, Result};
 use flatfs::Flatfs;
 
 fn main() -> Result<()> {
     let mut args = env::args();
-    let path = args.nth(1).unwrap();
+    let iter = args.nth(1).unwrap().trim().to_lowercase();
+    let path = args.next().unwrap().trim().to_string();
     let n: usize = args.next().unwrap().parse()?;
 
     println!("Opening {:?}", path);
@@ -13,11 +14,27 @@ fn main() -> Result<()> {
     let flatfs = Flatfs::new(&path)?;
     println!("Size on disk: {} bytes", flatfs.disk_usage());
 
-    // read n files serially
-    for r in flatfs.iter().take(n) {
-        let (key, value) = r?;
-        println!("{}", key);
-        println!("{:?}", value);
+    match iter.as_str() {
+        "all" => {
+            for r in flatfs.iter().take(n) {
+                let (key, value) = r?;
+                println!("{}", key);
+                println!("{:?}", value);
+            }
+        }
+        "keys" => {
+            for r in flatfs.keys().take(n) {
+                let key = r?;
+                println!("{}", key);
+            }
+        }
+        "values" => {
+            for r in flatfs.values().take(n) {
+                let value = r?;
+                println!("{:?}", value);
+            }
+        }
+        _ => return Err(eyre!("Unsupported action: {}", iter)),
     }
 
     Ok(())
